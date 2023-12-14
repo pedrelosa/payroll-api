fb3864d3afd768f87e973e4b23ac24e8a391df51
Pedrelosa <pedrelosa@hotmail.com>
2023-12-13 21:11:03 -0300

    feat- the PayrollResource is working dynamically

--- src/main/java/br/com/pedrelosa/payrollapi/resources/PayrollResource.java
+++ src/main/java/br/com/pedrelosa/payrollapi/resources/PayrollResource.java
@@ -1,22 +1,31 @@
 package br.com.pedrelosa.payrollapi.resources;
 
 import br.com.pedrelosa.payrollapi.domain.Payroll;
+import br.com.pedrelosa.payrollapi.domain.User;
+import br.com.pedrelosa.payrollapi.feignClients.UserFeign;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/payments")
+@RequiredArgsConstructor
 public class PayrollResource {
+
+    private final UserFeign userFeign;
 
     @GetMapping(value = "/{workerid}")
     public ResponseEntity<Payroll> getPayment(@PathVariable Long workerid, @RequestBody Payroll payment){
+
+        User user = userFeign.findById(workerid).getBody();
+
         return ResponseEntity.ok().body(
                 new Payroll(
-                        "Saulo",
+                        user.getName(),
                         payment.getDescription(),
-                        payment.getHourlyPrice(),
-                        160.0,
-                        payment.getHourlyPrice() * payment.getWorkedHours())
+                        user.getHourlyPrice(),
+                        payment.getWorkedHours(),
+                        user.getHourlyPrice() * payment.getWorkedHours())
         );
     }
 
